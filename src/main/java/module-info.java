bc638891395ca28ef5ae4fc5ecb330991c95a79b
Michael Hombauer <hombauer@lkw-walter.com>
2018-05-15 15:25:44 +0200

    Add reproducer

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module selenium.java10.reproducer {
+
+    exports my.module;
+
+    requires selenium.api;
+    requires selenium.chrome.driver;
+    requires selenium.remote.driver;
+
+}
---
+++ src/main/java/my/module/Dummy.java
@@ -0,0 +1,9 @@
+package my.module;
+
+public class Dummy {
+
+    public static void main(String... args) {
+        System.out.println("This is just a dummy to have a compilable file");
+    }
+
+}
